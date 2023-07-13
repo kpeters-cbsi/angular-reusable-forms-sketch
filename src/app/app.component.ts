8b10a5a77e8eade4b5aac7b22a689c0255fdc191
Aurelia Peters <aurelia.peters@cbsinteractive.com>
2023-07-13 16:51:08 -0500

    Remove some extraneous debugging

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
+/** @format */
+
+import { Component, OnInit } from "@angular/core"
+import { CommonModule } from "@angular/common"
 import {
   ReactiveFormsModule,
   FormControl,
   FormGroup,
   Validators,
   FormArray,
-} from '@angular/forms';
-import { FormFieldComponent } from './form-field.component';
-import { MatCardModule } from '@angular/material/card';
-import { TextFieldModule } from '@angular/cdk/text-field';
-import { FormArrayComponent } from './form-array-component/form-array.component';
+} from "@angular/forms"
+import { FormFieldComponent } from "./form-field.component"
+import { MatCardModule } from "@angular/material/card"
+import { TextFieldModule } from "@angular/cdk/text-field"
+import { FormArrayComponent } from "./form-array-component/form-array.component"
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
   standalone: true,
   imports: [
     CommonModule,
@@ -36,42 +38,42 @@ export class AppComponent implements OnInit {
     array: new FormArray<FormControl<String | null>>([], {
       validators: [Validators.minLength(2), Validators.maxLength(4)],
     }),
-  });
+  })
 
   ngOnInit() {
-    console.info('AppComponent::OnInit()', { array: this.array });
-    this.array.controls.push(new FormControl('Blarg') as FormControl<String>);
+    console.info("AppComponent::OnInit()", { array: this.array })
+    this.array.controls.push(new FormControl("") as FormControl<String>)
   }
 
   readonly errorMessages = {
-    required: 'This field is required, mac',
+    required: "This field is required",
     minlength: ({
       requiredLength,
       actualLength,
     }: {
-      requiredLength: number;
-      actualLength: number;
+      requiredLength: number
+      actualLength: number
     }) =>
       `There are only ${actualLength} items. There must be at least ${requiredLength}`,
     maxlength: ({
       requiredLength,
       actualLength,
     }: {
-      requiredLength: number;
-      actualLength: number;
+      requiredLength: number
+      actualLength: number
     }) =>
       `There are only ${actualLength} items. There must be at most ${requiredLength}`,
-  };
+  }
   get quantity() {
-    return this.form.get('quantity') as FormControl<Number | null>;
+    return this.form.get("quantity") as FormControl<Number | null>
   }
   get type() {
-    return this.form.get('type') as FormControl<String | null>;
+    return this.form.get("type") as FormControl<String | null>
   }
   get textarea() {
-    return this.form.get('textarea') as FormControl<String | null>;
+    return this.form.get("textarea") as FormControl<String | null>
   }
   get array() {
-    return this.form.get('array') as FormArray<FormControl<String>>;
+    return this.form.get("array") as FormArray<FormControl<String>>
   }
 }
--- src/app/form-field.component.ts
+++ src/app/form-field.component.ts
@@ -1,3 +1,5 @@
+/** @format */
+
 import {
   Component,
   Input,
@@ -5,23 +7,24 @@ import {
   ViewChild,
   Optional,
   Self,
-} from '@angular/core';
-import { FormsModule, NgControl } from '@angular/forms';
-import { CommonModule } from '@angular/common';
+} from "@angular/core"
+import { FormsModule, NgControl } from "@angular/forms"
+import { CommonModule } from "@angular/common"
 
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { CdkTextareaAutosize } from '@angular/cdk/text-field';
-import { TextFieldModule } from '@angular/cdk/text-field';
-import { take } from 'rxjs/operators';
-import { BaseFormField } from './base-form-field';
+import { MatFormFieldModule } from "@angular/material/form-field"
+import { MatInputModule } from "@angular/material/input"
+import { CdkTextareaAutosize } from "@angular/cdk/text-field"
+import { TextFieldModule } from "@angular/cdk/text-field"
+import { take } from "rxjs/operators"
+import { BaseFormField } from "./base-form-field"
 import {
   ErrorMessageContainerComponent,
   ErrorMessages,
-} from './error-message-container/error-message-container.component';
+} from "./error-message-container/error-message-container.component"
 @Component({
-  selector: 'app-form-field',
-  templateUrl: './form-field.component.html',
+  selector: "app-form-field",
+  templateUrl: "./form-field.component.html",
+  styles: [".mat-mdc-form-field { display: block; }"],
   standalone: true,
   imports: [
     CommonModule,
@@ -37,47 +40,47 @@ export class FormFieldComponent<T> extends BaseFormField {
     @Optional() @Self() public override controlDir: NgControl,
     private _ngZone: NgZone
   ) {
-    super(controlDir);
+    super(controlDir)
   }
 
-  @ViewChild('autosize') autosize!: CdkTextareaAutosize;
+  @ViewChild("autosize") autosize!: CdkTextareaAutosize
 
   ngOnInit(): void {
-    console.log('Entering FormFieldComponent::OnInit');
+    console.log("Entering FormFieldComponent::OnInit")
     // this.setComponentControl();
 
-    this.placeholder ??= null;
-    this.errorMessages ??= {};
-    this.required ??= !!this.required;
-    this.readonly ??= !!this.readonly;
-    this.value ??= null;
-    this.type ??= 'text';
+    this.placeholder ??= null
+    this.errorMessages ??= {}
+    this.required ??= !!this.required
+    this.readonly ??= !!this.readonly
+    this.value ??= null
+    this.type ??= "text"
   }
 
   @Input()
-  override value: T | null = null;
+  override value: T | null = null
   @Input()
-  placeholder?: string | null;
+  placeholder?: string | null
 
   @Input()
-  readonly?: boolean = false;
+  readonly?: boolean = false
 
   @Input()
-  type?: string = 'text';
+  type?: string = "text"
 
-  @Input() override label!: string;
+  @Input() override label!: string
   // giving the possibility to override the default error messages
-  @Input() override errorMessages: ErrorMessages = {};
+  @Input() override errorMessages: ErrorMessages = {}
 
   triggerResize() {
     // Wait for changes to be applied, then trigger textarea resize.
     this._ngZone.onStable
       .pipe(take(1))
-      .subscribe(() => this.autosize.resizeToFitContent(true));
+      .subscribe(() => this.autosize.resizeToFitContent(true))
   }
 
   doChange(value: any) {
-    console.debug('doChange()', { value });
-    this.writeValue(value);
+    console.debug("doChange()", { value })
+    this.writeValue(value)
   }
 }
