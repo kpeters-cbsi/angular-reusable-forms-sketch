e3acf772623bfa7631e6a92b287ea87644867430
Aurelia Peters <aurelia.peters@cbsinteractive.com>
2023-07-17 11:38:59 -0500

    Remove .spec files
    Realistically speaking, I'm not using them in this sketch

--- src/app/form-fields/dynamic-form/form-field-base.spec.ts
+++
@@ -1,7 +0,0 @@
-import { FormFieldBase } from './form-field-base';
-
-describe('FormFieldBase', () => {
-  it('should create an instance', () => {
-    expect(new FormFieldBase()).toBeTruthy();
-  });
-});
